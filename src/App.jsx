e47fe6eb7abadcd0eddb9f3ce3a3aec59939220f
Roshan Nasseri <roshan.sep26@gmail.com>
2021-07-17 05:35:47 +0000

    create css files and organize components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
-import HeroSection from './components/HeroSection';
-import Red from './components/Red';
-import Yellow from './components/Yellow';
-import Perks from './components/Perks';
-import Reviews from './components/Reviews';
-import GetExpCon from './components/GetExpCon';
-import Pricing from './components/Pricing';
-import Payments from './components/Payments';
+import Home from './components/Home/Home';
+import HeroSection from './components/HeroSection/HeroSection';
+import Red from './components/Red/Red';
+import Yellow from './components/Yellow/Yellow';
+import Perks from './components/Perks/Perks';
+import Reviews from './components/Reviews/Reviews';
+import GetExpCon from './components/GetExpCon/GetExpCon';
+import Pricing from './components/Pricing/Pricing';
+import Payments from './components/Payments/Payments';
 
 export default function App() {
   return (
