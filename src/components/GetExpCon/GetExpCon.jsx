5ec7c27e025ec397015a76651b64ed80ecc6597b
roshan0926 <80285716+roshan0926@users.noreply.github.com>
2021-07-19 14:41:36 -0700

    create get exp con component

--- src/components/GetExpCon/GetExpCon.jsx
+++ src/components/GetExpCon/GetExpCon.jsx
@@ -1,5 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import './getExpCon.scss';
+
 export default function GetExpCon() {
-  return (
-    <div>GetExpCon</div>
+  return (<>
+    <div className="getExpCon-page">
+      <a className="try-it-getExpCon" href="/pricing" data-text="Try it now">Try it now</a>
+      <h1 className="GetExpCon-title">get exp│con now</h1>
+      <p className="GetExpCon-subtitle">purchase and download the app</p>
+      <nav className="hamburger-getExpCon" role="navigation">
+        <p className="logo-getExpCon">EXP│CON</p>
+        <div id="menuToggle-getExpCon">
+          <input type="checkbox" />
+          <span></span>
+          <span></span>
+          <span></span>
+          <ul id="menu-getExpCon">
+            <a href="/home"><li>Home</li></a>
+            <a href="/getExpCon"><li>Red</li></a>
+            <a href="/perks"><li>Perks</li></a>
+            <a href="/reviews"><li>Reviews</li></a>
+            <a href="/yellow"><li>Yellow</li></a>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </>
   );
 };
