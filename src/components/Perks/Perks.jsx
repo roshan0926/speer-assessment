87284ea37602f9e31643170ef2a4dbcb6746bdc8
roshan0926 <80285716+roshan0926@users.noreply.github.com>
2021-07-18 20:20:44 -0700

    create the perks component (menu, button, title & subtitles)

--- src/components/Perks/Perks.jsx
+++ src/components/Perks/Perks.jsx
@@ -1,5 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import './perks.scss';
+
 export default function Perks() {
-  return (
-    <div>Perks</div>
+  return (<>
+    <div className="perks-page">
+    <a className="try-it-perks" href="/pricing" data-text="Try it now">Try it now</a>
+    <h1 className="perks-title">perks</h1>
+    <span className="line one"></span>
+    <h2 className="perk-subtitle" id="red">subscription payment model</h2>
+    <span className="line two"></span>
+    <h2 className="perk-subtitle" id="blue">no fee cancelation policy</h2>
+    <span className="line three"></span>
+    <h2 className="perk-subtitle" id="yellow">subscription payment model</h2>
+    <nav className="hamburger-perks" role="navigation">
+          <p className="logo-perks">EXP|CON</p>
+        <div id="menuToggle-perks">
+          <input type="checkbox" />
+          <span></span>
+          <span></span>
+          <span></span>
+          <ul id="menu-perks">
+            <a href="/home"><li>Home</li></a>
+            <a href="/perks"><li>Red</li></a>
+            <a href="/perks"><li>Perks</li></a>
+            <a href="/reviews"><li>Reviews</li></a>
+            <a href="/yellow"><li>Yellow</li></a>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </>
   );
 };
