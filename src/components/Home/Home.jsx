eebecdd9b5e9ce087577881c55782f13d114f04a
roshan0926 <80285716+roshan0926@users.noreply.github.com>
2021-07-19 19:17:03 -0700

    sp err

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import "./home.scss"
 export default function Home() {
   return (
     <div className="full-home-page">
-      <h1 className="first-h1">Hello and welcome to my speet technologies front end assessment</h1>
+      <h1 className="first-h1">Hello and welcome to my speer technologies front end assessment</h1>
       <h2 className="home-page-body">While I was creating this app I had not noticed only until I deployed my app to heroku that I was working on a screen at 67% zoom
         For this reason if you are viewing it on a screen around 1200px to zoom-out to 50% and on a screen of around 1500px to zoom-out to 67% and if a bit begger than that around 80% zoom
         Of course If I were to be working on production level code I would have the additional time to make it interactive baced on screen sizes.
